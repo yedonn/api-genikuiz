2b1487cc726482c1a931c7642a6e2f546d13d1c4
Yed Raoul Severin SEKE <yedraoulseverinseke@MacBook-Air-de-Yed.local>
2024-01-15 04:41:52 +0000

    docker

--- api/settings.py
+++ api/settings.py
@@ -3,7 +3,7 @@ from decouple import config
 class Envs:
     APP_NAME=config('DEBUG', default="API TCHAT")
 
-    DB_HOST='127.0.0.1'
+    DB_HOST='195.110.34.74'
     DB_PORT='28025'
     DB_USER='root'
     DB_PASSWORD='root'
